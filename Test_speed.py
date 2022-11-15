906f83bdaf43676a3488977c1aba5b5be0b03952
{KhaNguyen13} <nguyenthuckha13@gmail.com>
2022-11-15 17:44:31 +0700

    modified:   Test_speed.py

--- Test_speed.py
+++ Test_speed.py
@@ -10,21 +10,22 @@ st = speedtest.Speedtest()
 # print("Download: ", bytes_to_mb(st.download()), "Mb/s")
 # print("Upload: ", bytes_to_mb(st.upload()), "Mb/s")
 
+# downs = "Download speed: " + str(bytes_to_mb(st.download())) + " Mbp/s"
+# up = "Upload speed: " + str(bytes_to_mb(st.upload())) + " Mbp/s"
+
+downs = "Download"
+up = "Upload"
+
 windows = Tk() # Tao cua so gui
 windows.title("Title - Speed Test - Kha")
 windows.geometry("350x200") # Size gui
 
-# #Thêm label có nội dung Hello, font chữ
-down_lable = Label(windows, text=down_lable)
-# #Xác định vị trí của label
-down_lable.grid(column=0, row=0)
+# Them lable noi dung download/upload
+down_lable = Label(windows, text=downs).grid(column=0, row=0, sticky=W)
+up_lable = Label(windows, text=up).grid(column=0, row=1, sticky=W)
 
-# lbl = Label(windows, text="Hello")
-# lbl.grid(column=0, row=0)
-# #Thêm một nút nhấn Click Me
-# btn = Button(windows, text="Click Me", bg="orange", fg="red")
-# #Thiết lập vị trí của nút nhấn có màu nền và màu chữ
-# btn.grid(column=1, row=0)
+#Thiết lập vị trí của nút nhấn có màu nền và màu chữ
+button_check = Button(windows, text="Check !!!", bg="orange", fg="red").grid(column=0, row=2)
 
 
 windows.mainloop() # Lap vo tan de hien thi cua so
